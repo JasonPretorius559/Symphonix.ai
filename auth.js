d6667464b6734ee0ae9e03aafd88da6bf3e8e501
Jason <pretoriusjason@gmail.com>
2024-09-17 12:51:34 +0200

    management console imp

--- auth.js
+++ auth.js
@@ -1,19 +1,28 @@
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session); // Use express-mysql-session for session storage
+const connection = require('./db'); // Import the connection pool from db.js
 
-// Middleware configuration for sessions with in-memory store
+// Configure MySQL session store using the connection pool from db.js
+const sessionStore = new MySQLStore({
+  expiration: 24 * 60 * 60 * 1000, // Session expiration time (1 day)
+  endConnectionOnClose: false,     // Keep the connection alive
+}, connection); // Use the MySQL connection pool directly
+
+// Middleware configuration for sessions with MySQL store
 const sessionMiddleware = session({
   secret: '777777777', // Replace with your own secret key
   resave: false,
   saveUninitialized: false,
+  store: sessionStore, // Use MySQL session store
   cookie: {
     secure: false, // Set to true if using HTTPS
     maxAge: 24 * 60 * 60 * 1000 // Cookie expiration time (1 day)
   }
 });
 
+
 // Middleware to check authentication
 const Authenticated = (req, res, next) => {
-
   if (req.session.user) {
     return next(); // User is authenticated
   } else {
@@ -21,4 +30,13 @@ const Authenticated = (req, res, next) => {
   }
 };
 
-module.exports = { sessionMiddleware, Authenticated };
+const admin = (req, res, next) => {
+  if (req.session.user && req.session.user.role === 1) {
+    return next(); // User is admin
+  } else {
+    res.redirect('/home'); // Redirect if not admin
+  }
+};
+
+
+module.exports = { sessionMiddleware, Authenticated, admin };
